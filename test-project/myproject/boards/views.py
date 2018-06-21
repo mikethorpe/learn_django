7bf482c67fd2c56ab7f3945dfe44a503e4b1633e
Unknown <michael.a.thorpe@gmail.com>
2018-06-21 09:27:47 +0100

    Intermediate commit

--- test-project/myproject/boards/views.py
+++ test-project/myproject/boards/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Board
+from django.http import Http404
 # Create your views here.
 
 from django.http import HttpResponse
@@ -9,3 +10,6 @@ def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
 
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
